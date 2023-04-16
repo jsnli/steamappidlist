0267cefe61f75f93a82def71f183ab886bf1c11f
Jason <130726285+jsnli@users.noreply.github.com>
2023-04-16 05:41:44 -0400

    recurse requests for response limit

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const core = require('@actions/core');
 const fs = require('fs');
-
-const url = `https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.STEAM_KEY}&include_games=true`;
-
+const baseURL = "https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.STEAM_KEY}&include_games=true&include_dlc=false&include_software=false&include_videos=false&include_hardware=false&max_results=50000";
 
 async function run() {
+    let outputJSON = {
+        apps: []
+    };
     try {
-        const response = await fetch(url);
-        const jsonData = await response.json();
-        const jsonString = JSON.stringify(jsonData.response);
-
-        fs.writeFileSync('data.json', jsonString, (error) => {
+        let have_more_results = true;
+        let last_appid = null;
+        while (have_more_results) {
+            let appList = await getAppList(last_appid);
+            outputJSON.apps = outputJSON.apps.concat(appList.response.apps);
+            have_more_results = appList.response.have_more_results;
+            last_appid = appList.response.last_appid;
+        }
+        fs.writeFileSync('data.json', JSON.stringify(outputJSON), (error) => {
             if (error) throw error;
         });
     } catch (error) {
@@ -18,4 +23,17 @@ async function run() {
     }
 }
 
+async function getAppList(last_appid) {
+    // api limits results to 50000 with a last_appid returned for continuation
+    // if last_appid, attach parameter to continue fetching results
+    let url = baseURL;
+    if (last_appid) {
+        url = `${url}&last_appid=${last_appid}`
+    }
+
+    const response = await fetch(url);
+    const jsonData = await response.json();
+
+    return jsonData;
+}
 run();
