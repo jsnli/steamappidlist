df702520e44e4729dfa54c7bec0a93e79af8e876
Jason <130726285+jsnli@users.noreply.github.com>
2023-04-16 04:35:53 -0400

    adjust output format. add missing semicolon

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const core = require('@actions/core');
 const fs = require('fs');
 
-const url = `https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.STEAM_KEY}&include_games=true`
+const url = `https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.STEAM_KEY}&include_games=true`;
+
 
 async function run() {
     try {
         const response = await fetch(url);
         const jsonData = await response.json();
-        const jsonString = JSON.stringify(jsonData.response.apps);
+        const jsonString = JSON.stringify(jsonData.response);
 
         fs.writeFileSync('data.json', jsonString, (error) => {
             if (error) throw error;
