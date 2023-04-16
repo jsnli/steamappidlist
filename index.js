03dce772d6972ab1c0e351f7673bd9d83a757e3b
Jason <130726285+jsnli@users.noreply.github.com>
2023-04-16 03:13:15 -0400

    add steam key enviornment variable and adjust fetch/write request

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const core = require('@actions/core');
 const fs = require('fs');
 
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+const url = `https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.STEAM_KEY}&include_games=true`
 
 async function run() {
     try {
         const response = await fetch(url);
         const jsonData = await response.json();
-        fs.writeFileSync('data.json', JSON.stringify(jsonData), (error) => {
+        const jsonString = JSON.stringify(jsonData.response.apps);
+        fs.writeFileSync('data.json', JSON.stringify(jsonString), (error) => {
             if (error) throw error;
         });
     } catch (error) {
