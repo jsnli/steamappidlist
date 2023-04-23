d89b22b047095e4ccb2d5e7609a1d499e4ae034b
Jason <130726285+jsnli@users.noreply.github.com>
2023-04-22 20:25:46 -0400

    update for non-game dataset creation

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const core = require('@actions/core');
 const fs = require('fs');
-const baseURL = `https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.STEAM_KEY}&include_games=true&include_dlc=false&include_software=false&include_videos=false&include_hardware=false&max_results=50000`;
 
+const key = process.env.STEAM_KEY;
+const baseURL = "https://api.steampowered.com/IStoreService/GetAppList/v1/?";
+const categories = ["games", "dlc", "software", "videos", "hardware"];
+
+/**
+ * Entry point for script
+ *
+ */
 async function run() {
-    let outputJSON = {
-        apps: []
-    };
-    try {
-        let have_more_results = true;
-        let last_appid = null;
-        while (have_more_results) {
-            let appList = await getAppList(last_appid);
-            outputJSON.apps = outputJSON.apps.concat(appList.response.apps);
-            have_more_results = appList.response.have_more_results;
-            last_appid = appList.response.last_appid;
-        }
-        fs.writeFileSync('data.json', JSON.stringify(outputJSON), (error) => {
-            if (error) throw error;
-        });
-    } catch (error) {
-        core.setFailed(error.Message);
-    }
+    categories.forEach(function(element) {
+        requestData(element);
+    });
 }
 
-async function getAppList(last_appid) {
+/**
+ * Returns x raised to the n-th power.
+ *
+ * @param {number} last_appid appID of last entry. used to continue in subsequent calls.
+ * @param {string} endpoint The power, must be a natural number.
+ * @return {json} data containing app id and app name
+ */
+async function getAppList(last_appid, endpoint) {
     // api limits results to 50000 with a last_appid returned for continuation
-    // if last_appid, attach parameter to continue fetching results
-    let url = baseURL;
+    let url = endpoint;
     if (last_appid) {
         url = `${url}&last_appid=${last_appid}`
     }
@@ -36,4 +34,41 @@ async function getAppList(last_appid) {
 
     return jsonData;
 }
+
+/**
+ * makes requests for selected category
+ *
+ * @param {string} category category of app id to be returned
+ * @return void
+ */
+async function requestData(category) {
+    const gameParam =`&include_games=${category == "games"}`;
+    const dlcParam =`&include_dlc=${category == "dlc"}`;
+    const softwareParam =`&include_software=${category == "software"}`;
+    const videosParam =`&include_videos=${category == "videos"}`;
+    const hardwareParam =`&include_hardware=${category == "hardware"}`;
+
+    const endpoint = `${baseURL}key=${key}${gameParam}${dlcParam}${softwareParam}${videosParam}${hardwareParam}&max_results=50000`;
+
+
+    let outputJSON = {
+        apps: []
+    };
+    try {
+        let have_more_results = true;
+        let last_appid = null;
+        while (have_more_results) {
+            let appList = await getAppList(last_appid, endpoint);
+            outputJSON.apps = outputJSON.apps.concat(appList.response.apps);
+            have_more_results = appList.response.have_more_results;
+            last_appid = appList.response.last_appid;
+        }
+        fs.writeFileSync(`./data/${category}_appid.json`, JSON.stringify(outputJSON), (error) => {
+            if (error) throw error;
+        });
+    } catch (error) {
+        core.setFailed(error.Message);
+    }
+}
+
 run();
