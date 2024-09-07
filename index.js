740fac6f04fcbd12d60dc1151d6434612e2ce0dc
Jason <130726285+jsnli@users.noreply.github.com>
2024-09-07 01:54:52 -0400

    adjust comments, remove apps key

--- index.js
+++ index.js
@@ -5,25 +5,14 @@ const key = process.env.STEAM_KEY;
 const baseURL = "https://api.steampowered.com/IStoreService/GetAppList/v1/?";
 const categories = ["games", "dlc", "software", "videos", "hardware"];
 
-/**
- * Entry point for script
- *
- */
 async function run() {
     categories.forEach(function(element) {
         requestData(element);
     });
 }
 
-/**
- * requests first or next set of appid based on last_appid
- *
- * @param {number} last_appid appID of last entry. used to continue in subsequent calls.
- * @param {string} endpoint The power, must be a natural number.
- * @return {object} data containing app id and app name
- */
 async function getAppList(last_appid, endpoint) {
-    // api limits results to 50000 with a last_appid returned for continuation
+    // api 50000 result limit. Last_appid returned for continuation
     let url = endpoint;
     if (last_appid) {
         url = `${url}&last_appid=${last_appid}`
@@ -35,12 +24,6 @@ async function getAppList(last_appid, endpoint) {
     return jsonData;
 }
 
-/**
- * makes requests for selected category
- *
- * @param {string} category category of app id to be returned
- * @return void
- */
 async function requestData(category) {
     const gameParam =`&include_games=${category == "games"}`;
     const dlcParam =`&include_dlc=${category == "dlc"}`;
@@ -51,15 +34,14 @@ async function requestData(category) {
     const endpoint = `${baseURL}key=${key}${gameParam}${dlcParam}${softwareParam}${videosParam}${hardwareParam}&max_results=50000`;
 
 
-    let outputJSON = {
-        apps: []
-    };
+    let outputJSON = {};
+
     try {
         let have_more_results = true;
         let last_appid = null;
         while (have_more_results) {
             let appList = await getAppList(last_appid, endpoint);
-            outputJSON.apps = outputJSON.apps.concat(appList.response.apps);
+            outputJSON= outputJSON.concat(appList.response.apps);
             have_more_results = appList.response.have_more_results;
             last_appid = appList.response.last_appid;
         }
